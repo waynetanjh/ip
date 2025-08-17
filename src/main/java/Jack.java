57b462651d7347107bffb407b3da9b0f155fd018
Wayne Tan <e1356977@u.nus.edu>
2025-08-18 02:13:41 +0800

    feat: echo commands entered by user and exit when user types bye

--- src/main/java/Jack.java
+++ src/main/java/Jack.java
@@ -1,15 +1,29 @@
+import javax.swing.plaf.metal.MetalFileChooserUI;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Jack {
+    private static void echo(String text) {
+        String line = "-----------------------------------------";
+        System.out.println("\t" + line);
+        System.out.println("\t" + text);
+        System.out.println("\t" + line);
+    }
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String name = "Jack";
         String line = "-----------------------------------------";
         System.out.println(line);
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
 
-
+        String userInput = scanner.nextLine();
+        while (!userInput.equals("bye")) {
+            echo(userInput);
+            userInput = scanner.nextLine();
+        }
+        echo("Bye. Hope to see you again soon!");
 
     }
 }
