9bd95fc3677386f63ec2655a049393c19e810d4e
Wayne Tan <e1356977@u.nus.edu>
2025-08-18 01:46:24 +0800

    feat: initial skeletal version

---
+++ src/main/java/Jack.java
@@ -0,0 +1,15 @@
+public class Jack {
+    public static void main(String[] args) {
+        String name = "Jack";
+        String line = "-----------------------------------------";
+        System.out.println(line);
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+
+
+
+    }
+}
