e460ce682987b8668b958136a92a9fdfef1cda2b
Wayne Tan <e1356977@u.nus.edu>
2025-08-18 15:20:10 +0800

    ability to store text entered by the user and display them when requested.

--- src/main/java/Jack.java
+++ src/main/java/Jack.java
@@ -3,12 +3,15 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Jack {
+    private static final int MAX = 100;
+
     private static void echo(String text) {
         String line = "-----------------------------------------";
         System.out.println("\t" + line);
-        System.out.println("\t" + text);
+        System.out.println(text);
         System.out.println("\t" + line);
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String name = "Jack";
@@ -18,12 +21,29 @@ public class Jack {
         System.out.println("What can I do for you?");
         System.out.println(line);
 
+        String[] tasks = new String[MAX];
+        int size = 0;
+
         String userInput = scanner.nextLine();
-        while (!userInput.equals("bye")) {
-            echo(userInput);
+        while (true) {
+            if (userInput.equals("list") && size != 0) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < size; i++) {
+                    sb.append("\t").append((i + 1)).append(". ").append(tasks[i]);
+                    if (i < size - 1 ) {
+                        sb.append("\n");
+                    }
+                }
+                echo(sb.toString());
+            } else if (userInput.equals("bye")) {
+                echo("\t" + "Bye. Hope to see you again soon!");
+                break;
+            } else {
+                echo("\t" + userInput);
+                tasks[size] = userInput;
+                size++;
+            }
             userInput = scanner.nextLine();
         }
-        echo("Bye. Hope to see you again soon!");
-
     }
 }
