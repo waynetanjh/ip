88e3d6c991001920e0c18890fa6297fe43e2a113
Wayne Tan <e1356977@u.nus.edu>
2025-08-20 20:16:05 +0800

    Add delete functionality

--- src/main/java/Jack.java
+++ src/main/java/Jack.java
@@ -1,9 +1,8 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Jack {
-    private static final int MAX = 100;
-    private static final String LINE = "-----------------------------------------";;
-
+    private static final String LINE = "-----------------------------------------";
     private static void echo(String text) {
         System.out.println("\t" + LINE);
         System.out.println(text);
@@ -32,17 +31,27 @@ public class Jack {
         System.out.println(LINE);
     }
 
+    private static void handleDelete(ArrayList<Task> tasks, int index) {
+        if (tasks.isEmpty()) {
+            throw new IllegalArgumentException("No tasks to delete");
+        }
+        Task removed = tasks.remove(index);
+
+        echo("\tNoted. I've removed this task:\n\t  "
+                + removed
+                + "\n\tNow you have " + tasks.size() + " tasks in the list.");
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         printWelcomeMessage();
 
-        Task[] tasks = new Task[MAX];
-        int numberOfTasks = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
 
         while (true) {
             String userInput = scanner.nextLine();
-            if (userInput.equals("")) {
+            if (userInput.isEmpty()) {
                 echo("\tEnter a valid task\n\t");
                 continue;
             }
@@ -53,13 +62,13 @@ public class Jack {
             }
 
             if (userInput.equals("list")) {
-                if (numberOfTasks == 0) {
+                if (tasks.isEmpty()) {
                     echo("\tHere are the tasks in your list:\n\t  (no tasks yet)");
                 } else {
                     StringBuilder sb = new StringBuilder();
-                    for (int i = 0; i < numberOfTasks; i++) {
-                        sb.append("\t").append(i + 1).append(". ").append(tasks[i].toString());
-                        if (i < numberOfTasks - 1) {
+                    for (int i = 0; i < tasks.size(); i++) {
+                        sb.append("\t").append(i + 1).append(". ").append(tasks.get(i).toString());
+                        if (i < tasks.size()- 1) {
                             sb.append("\n");
                         }
                     }
@@ -71,35 +80,34 @@ public class Jack {
                 break;
             } else if (userInput.startsWith("mark")) {
                 int index = Integer.parseInt(userInput.split(" ")[1]) - 1;
-                tasks[index].completed();
-                markCompleted("\t" + tasks[index].toString());
+                tasks.get(index).completed();
+                markCompleted("\t" + tasks.get(index).toString());
             } else if (userInput.startsWith("unmark")) {
                 int index = Integer.parseInt(userInput.split(" ")[1]) - 1;
-                tasks[index].unmark();
-                unmarkCompleted("\t" + tasks[index].toString());
+                tasks.get(index).unmark();
+                unmarkCompleted("\t" + tasks.get(index).toString());
             } else if (userInput.startsWith("todo")) {
 
                 // Error handling for empty description of todo
-                if (userInput.length() <= 4) {
+                if (userInput.length() == 4) {
                     echo("\tOOPS!!! The description of a todo cannot be empty");
                     continue;
                 }
                 String desc = userInput.substring(5).trim(); // everything after "todo"
-                tasks[numberOfTasks] = new Todo(desc);
-                numberOfTasks++;
+                tasks.add(new Todo(desc));
 
                 echo("\tGot it. I've added this task:\n\t  "
-                        + tasks[numberOfTasks - 1]
-                        + "\n\tNow you have " + numberOfTasks + " tasks in the list.");
+                        + tasks.get(tasks.size() - 1)
+                        + "\n\tNow you have " + tasks.size() + " tasks in the list.");
             } else if (userInput.startsWith("deadline")) {
                 String[] parts = userInput.substring(9).trim().split("/by", 2);
                 String desc = parts[0].trim();
                 String by = parts[1].trim();
-                tasks[numberOfTasks] = new Deadline(desc, by);
-                numberOfTasks++;
+                tasks.add(new Deadline(desc, by));
+                int numberOfTasks = tasks.size();
 
                 echo("\tGot it. I've added this task:\n\t  "
-                        + tasks[numberOfTasks - 1]
+                        + tasks.get(numberOfTasks - 1)
                         + "\n\tNow you have " + numberOfTasks + " tasks in the list.");
 
             } else if (userInput.startsWith("event")) {
@@ -108,17 +116,19 @@ public class Jack {
                 String desc = parts[0].trim();
                 String from = parts[1].trim();
                 String to = parts[2].trim();
-                tasks[numberOfTasks] = new Event(desc, from, to);
-                numberOfTasks++;
+                tasks.add(new Event(desc, from, to));
+                int numberOfTasks = tasks.size();
 
                 echo("\tGot it. I've added this task:\n\t  "
-                        + tasks[numberOfTasks - 1]
+                        + tasks.get(numberOfTasks - 1)
                         + "\n\tNow you have " + numberOfTasks + " tasks in the list.");
 
+            } else if (userInput.startsWith("delete")) {
+                int index = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                handleDelete(tasks, index);
             } else {
                 echo("\t" + userInput);
-                tasks[numberOfTasks] = new Task(userInput);
-                numberOfTasks++;
+                tasks.add(new Task(userInput));
             }
         }
         scanner.close();
